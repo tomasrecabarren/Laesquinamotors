10a909db9e5f242363fc9573a17802964c93e308
Tomas Recabarren <tomas.recabarren.toro@alumnos.sip.cl>
2024-09-23 12:09:54 -0300

    primeros cambios

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import flask, render_template
-app = flask (__name__)
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
