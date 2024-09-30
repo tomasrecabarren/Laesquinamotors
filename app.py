a2fca1f3fc1701fa5cc80ae83c6fc4d274bd77d4
Tomas Recabarren <tomas.recabarren.toro@alumnos.sip.cl>
2024-09-30 12:20:52 -0300

    proceso

--- app.py
+++ app.py
@@ -6,5 +6,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/registro')
+def registro():
+    return render_template('registro.html') 
+
+#////////////////////////////////////// Autos
+@app.route('/mazda')
+def mazda():
+    return render_template('mazda.html') 
+
+@app.route('/suzuki')
+def suzuki():
+    return render_template('suzuki.html') 
+
+@app.route('/supra')
+def supra():
+    return render_template('supra.html')
+
+@app.route('/Mirage')
+def mirage():
+    return render_template('mirage.html') 
+
+#////////////////////////////////////////
+
 if __name__ == '__main__':
     app.run(debug=True)
